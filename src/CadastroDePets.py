81f97297dd18b2c14b3d3f9176a9dc3cd9d2335c
Yago <yagotavares199@gmail.com>
2023-06-06 19:37:24 -0300

    Adicionando cadastro de pet

--- src/CadastroDePets.py
+++ src/CadastroDePets.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QComboBox
 from PyQt5.QtGui import QPixmap, QIcon
 
 
@@ -66,12 +66,78 @@ class TelaCadastro(QMainWindow):
         self.email_edit.setFixedWidth(200)
         self.email_edit.setStyleSheet('font-size: 15px')
 
-        self.email_label = QLabel('Endereço:', self)
-        self.email_label.move(15, 200)
-        self.email_label.setStyleSheet('font-size: 20px')
+        self.endereco_label = QLabel('Endereço:', self)
+        self.endereco_label.move(15, 200)
+        self.endereco_label.setStyleSheet('font-size: 20px')
 
-        self.email_edit = QLineEdit(self)
-        self.email_edit.move(120, 200)
-        self.email_edit.setFixedSize(230, 30)
-        self.email_edit.setFixedWidth(445)
-        self.email_edit.setStyleSheet('font-size: 15px')
+        self.endereco_edit = QLineEdit(self)
+        self.endereco_edit.move(120, 200)
+        self.endereco_edit.setFixedSize(230, 30)
+        self.endereco_edit.setFixedWidth(445)
+        self.endereco_edit.setStyleSheet('font-size: 15px')
+# ----------------------------------------------------------------------------------------------------------------------
+        self.Dados_Pessoais = QLabel('Dados do Pet', self)
+        self.Dados_Pessoais.move(175, 260)
+        self.Dados_Pessoais.setFixedSize(300, 50)
+        self.Dados_Pessoais.setStyleSheet('font-size: 40px')
+
+        self.tipo_label = QLabel('Tipo:', self)
+        self.tipo_label.move(35, 330)
+        self.tipo_label.setStyleSheet('font-size: 20px')
+
+        self.combobox_tipo = QComboBox(self)
+        self.combobox_tipo.move(100, 330)
+        self.combobox_tipo.setFixedWidth(175)
+        self.combobox_tipo.setStyleSheet('font-size: 15px')
+        self.carregar_tipo()
+
+        self.raca_label = QLabel('Raça:', self)
+        self.raca_label.move(305, 330)
+        self.raca_label.setStyleSheet('font-size: 20px')
+
+        self.raca_edit = QLineEdit(self)
+        self.raca_edit.move(365, 330)
+        self.raca_edit.setFixedSize(230, 30)
+        self.raca_edit.setFixedWidth(200)
+        self.raca_edit.setStyleSheet('font-size: 15px')
+
+        self.idade_label = QLabel('Idade:', self)
+        self.idade_label.move(35, 400)
+        self.idade_label.setStyleSheet('font-size: 20px')
+
+        self.combobox_idade = QComboBox(self)
+        self.combobox_idade.move(100, 400)
+        self.combobox_idade.setFixedWidth(175)
+        self.combobox_idade.setStyleSheet('font-size: 15px')
+        self.carregar_idade()
+
+        self.vacina_label = QLabel('Vacina:', self)
+        self.vacina_label.move(300, 400)
+        self.vacina_label.setStyleSheet('font-size: 20px')
+
+        self.combobox_vacina = QComboBox(self)
+        self.combobox_vacina.move(375, 400)
+        self.combobox_vacina.setFixedWidth(190)
+        self.combobox_vacina.setStyleSheet('font-size: 15px')
+        self.carregar_vacina()
+
+    def carregar_tipo(self):
+        tipos = ['Caninos', 'Felinos', 'Outros']
+        for tipo in range(len(tipos)):
+            self.combobox_tipo.addItem(tipos[tipo])
+
+    def carregar_idade(self):
+        tipos = []
+        for c in range(1, 21):
+            tipos.append(str(c))
+        for tipo in range(len(tipos)):
+            self.combobox_idade.addItem(tipos[tipo])
+
+    def carregar_vacina(self):
+        tipos = ['Em dia', 'Ausente']
+        for tipo in range(len(tipos)):
+            self.combobox_vacina.addItem(tipos[tipo])
+
+
+
+
