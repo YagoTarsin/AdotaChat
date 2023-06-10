e0a1f38f3447bfc388efe59f86ba0ae36e8a277d
Yago <yagotavares199@gmail.com>
2023-06-10 19:11:03 -0300

    Ajustes

--- src/Terminal.py
+++ src/Terminal.py
@@ -1,6 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QDialog, QTextEdit, QVBoxLayout
-from PyQt5.QtGui import QFont, QColor, QPalette
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QTextEdit, QVBoxLayout
+from PyQt5.QtGui import QFont
 
 
 class TerminalDialog(QDialog):
